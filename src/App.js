65c55f943f751c1f83d7aef84883faace8ada0bc
Peter Kim <123peterkim@gmail.com>
2018-07-14 10:45:56 -0700

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import * as CalculatorButtons from "./CalculatorButtons";
 
 class Calculator extends Component {
   constructor(props) {
@@ -26,7 +27,7 @@ class Calculator extends Component {
     });
   }
   initialize() {
-    this.setState({ currentVal: 0 });
+    this.setState({ currentVal: "0" });
   }
   render() {
     return (
@@ -34,9 +35,9 @@ class Calculator extends Component {
         <Formula formula={this.state.currentVal} />
         <Output currentValue={this.state.currentVal} />
         <Buttons
-          numbers={this.handleNumbers}
+          number={this.handleNumbers}
           initialize={this.initialize}
-          operators={this.handleOperators}
+          operator={this.handleOperators}
         />
       </div>
     );
@@ -60,59 +61,17 @@ class Buttons extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.props.initialize} id="clear" value="ac">
-          AC
-        </button>
-        <button onClick={this.props.operators} id="divide" value="/">
-          /
-        </button>
-        <button onClick={this.props.operators} id="multiply" value="*">
-          *
-        </button>
-        <button onClick={this.props.numbers} id="seven" value="7">
-          7
-        </button>
-        <button onClick={this.props.numbers} id="eight" value="8">
-          8
-        </button>
-        <button onClick={this.props.numbers} id="nine" value="9">
-          9
-        </button>
-        <button onClick={this.props.operators} id="subtract" value="-">
-          -
-        </button>
-        <button onClick={this.props.numbers} id="four" value="4">
-          4
-        </button>
-        <button onClick={this.props.numbers} id="five" value="5">
-          5
-        </button>
-        <button onClick={this.props.numbers} id="six" value="6">
-          6
-        </button>
-
-        <button onClick={this.props.operators} id="add" value="+">
-          +
-        </button>
-
-        <button onClick={this.props.numbers} id="one" value="1">
-          1
-        </button>
-        <button onClick={this.props.numbers} id="two" value="2">
-          2
-        </button>
-        <button onClick={this.props.numbers} id="three" value="3">
-          3
-        </button>
-        <button onClick={this.props.operators} id="equals" value="=">
-          =
-        </button>
-        <button onClick={this.props.numbers} id="zero" value="0">
-          0
-        </button>
-        <button id="decimal" value=".">
-          .
-        </button>
+        {CalculatorButtons.sort((a, b) => a.order - b.order).map(button => (
+          <button
+            key={button.id}
+            onClick={this.props[button.type]}
+            id={button.id}
+            className={button.css.join(" ")}
+            value={button.value}
+          >
+            {button.value.toUpperCase()}
+          </button>
+        ))}
       </div>
     );
   }
